598eb96b9a1ff19c1a9e943d1adbbf96d06f9416
Alba Mendez <alba.mendez@dolby.com>
2023-11-02 21:58:14 +0100

    fix produced .d.ts
    - fix index.d.ts to properly use commonjs "export =" syntax
    - output the native .d.ts. while the end user should not
      need to call C++ methods, this way we reduce transitive
      typechecking errors.

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,16 @@
 export * from "./build/types/RTMPServer";
 
-export type { default as Server } from "./build/types/Server";
-export type { default as Application } from "./build/types/Application";
-export type { default as Client } from "./build/types/Client";
-export type { default as Stream, AMFData, Command } from "./build/types/Stream";
-export type { default as IncomingStreamBridge, StreamStats } from "./build/types/IncomingStreamBridge";
+export type Server = import("./build/types/Server");
+export type Application = import("./build/types/Application");
+export type Client = import("./build/types/Client");
+export type Stream = import("./build/types/Stream");
+export type IncomingStreamBridge = import("./build/types/IncomingStreamBridge");
+export type IncomingStreamTrackBridge = import("./build/types/IncomingStreamTrackBridge");
 
 export type {
-	default as IncomingStreamTrackBridge,
 	ActiveEncodingInfo, ActiveLayersInfo,
 	EncodingStats, LayerStats, MediaStats, PacketWaitTime, TrackStats,
 } from "./build/types/IncomingStreamTrackBridge";
+
+export type { AMFData, Command } from "./build/types/Stream";
+export type { StreamStats } from "./build/types/IncomingStreamBridge";
