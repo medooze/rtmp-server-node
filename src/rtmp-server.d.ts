d9496a6691bfabf31ea3c3fcd3bb130c1a5d7cf5
harryz2000 <119906558+harryz2000@users.noreply.github.com>
2024-07-03 17:07:35 +1000

    Expose FrameDispatchCoordinator (#44)

--- src/rtmp-server.d.ts
+++ src/rtmp-server.d.ts
@@ -240,6 +240,19 @@ export  class RTMPServerModule {
   constructor();
 }
 
+
+export class FrameDispatchCoordinator {
+    SetMaxDelayMs(maxDelayMs: number): void;
+}
+
+export  class FrameDispatchCoordinatorShared {
+  
+  constructor();
+  
+  get(): FrameDispatchCoordinator;
+}
+
+
 export  class MediaFrameListenerBridge extends RTPIncomingMediaStream {
 
   constructor(timeService: TimeService | EventLoop, ssrc: number);
@@ -287,6 +300,8 @@ export  class MediaFrameListenerBridge extends RTPIncomingMediaStream {
   RemoveMediaListener(listener: MediaFrameListenerShared): void;
 
   SetTargetBitrateHint(targetBitrateHint: number): void;
+  
+  SetFrameDispatchCoordinator(coordinator: FrameDispatchCoordinatorShared): void;
 
   codec: string;
 
